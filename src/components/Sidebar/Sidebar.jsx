a47baf05520f59b167dff1d904eb9936d352ecad
BeGeos <montepeloso.m@gmail.com>
2022-01-13 18:30:31 +0100

    Updated sidebar new items

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -13,26 +13,25 @@ function MenuItem({ children, title }) {
 export default function Sidebar() {
   return (
     <div className="sidebar">
-      <h2>Options</h2>
       <MenuItem title="Earnings">
         <Link to="/">
-          <li>Monthly</li>
-        </Link>
-        <Link to="/">
-          <li>Yearly</li>
+          <li>Overview</li>
         </Link>
       </MenuItem>
       <MenuItem title="Expenses">
         <Link to="/">
-          <li>All</li>
+          <li>Overview</li>
         </Link>
       </MenuItem>
-      <MenuItem title="Vaults">
+      <MenuItem title="Savings">
         <Link to="/vaults/savings">
-          <li>Savings</li>
+          <li>Overview</li>
+        </Link>
+        <Link to="/vaults/savings">
+          <li>Vaults</li>
         </Link>
         <Link to="/vaults/budget">
-          <li>Budget</li>
+          <li>Budgets</li>
         </Link>
       </MenuItem>
       <MenuItem title="Analytics">
@@ -43,18 +42,12 @@ export default function Sidebar() {
           <li>Compare</li>
         </Link>
       </MenuItem>
-      <MenuItem title="Category">
+      <MenuItem title="Payments">
         <Link to="/">
-          <li>Clothes</li>
+          <li>Invoices</li>
         </Link>
         <Link to="/">
-          <li>Food</li>
-        </Link>
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-        <Link to="/">
-          <li>Business</li>
+          <li>Bills</li>
         </Link>
       </MenuItem>
     </div>
